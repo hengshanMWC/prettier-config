b3e7f5abc12345d0620d3f86685734d32f5c91bf
mawencan <1044926436@qq.com>
2023-02-14 17:00:32 +0800

    build: 打包可控

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,23 @@
 /// <reference types="vitest/globals" />
 import { resolve } from 'path'
 import { defineConfig } from 'vitest/config'
+import validateNpmPackageName from 'validate-npm-package-name'
+import { pascalCase } from 'change-case'
+import { name } from './package.json'
+const moduleName = name
+// 检查是否是合法的 npm 包名
+if (!validateNpmPackageName(moduleName)) {
+  throw new Error(`${moduleName} 不是一个合法的 npm 包名`)
+}
+
 export default defineConfig({
   build: {
     lib: {
       entry: resolve(__dirname, './src/index.ts'),
-      name: 'Demo',
+      formats: ['es', 'umd'],
+      fileName: 'index',
+      // 将其他形式的命名规则转换为驼峰命名
+      name: pascalCase(moduleName),
     },
   },
   resolve: {
