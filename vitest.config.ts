3a290badd61124433d025b9414f060c91a3491e9
马文灿 <mawencan@cvte.com>
2023-06-27 14:54:33 +0800

    chore: 使用tsup代替webpack

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,14 +1,6 @@
 /// <reference types="vitest" />
 /// <reference types="vitest/globals" />
 import { defineConfig } from 'vitest/config'
-import validateNpmPackageName from 'validate-npm-package-name'
-import { name } from './package.json'
-
-const moduleName = name
-// 检查是否是合法的 npm 包名
-if (!validateNpmPackageName(moduleName)) {
-  throw new Error(`${moduleName} 不是一个合法的 npm 包名`)
-}
 
 export default defineConfig({
   test: {
