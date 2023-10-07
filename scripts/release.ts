3540ff4accd61ad0cf71b3614861a447e473fbb3
mawencan <1819293933@qq.com>
2023-10-07 15:45:06 +0800

    chore: 用pkgs发版本

--- scripts/release.ts
+++ scripts/release.ts
@@ -1,10 +1,12 @@
 import { execSync } from 'node:child_process'
 import colors from 'colors'
+import { commandPublish, commandVersion } from '@abmao/pkgs'
 
 console.log(`${colors.cyan.bold('release: start')} 🏗`);
 (async function () {
   execSync('npm run test', { stdio: 'inherit' })
   execSync('npm run build', { stdio: 'inherit' })
-  // await versionBump()
+  await commandVersion()
+  await commandPublish()
 })()
 console.log(`${colors.cyan.bold('release: success')} 🎉🎉🎉🎉🎊`)
