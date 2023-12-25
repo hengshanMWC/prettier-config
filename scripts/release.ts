e30e4f8506c9994756f678c85a6f2c71f2bccf9b
hengshanMWC <1819293933@qq.com>
2023-12-25 16:29:08 +0800

    fix(chore): 删除build相关

--- scripts/release.ts
+++ scripts/release.ts
@@ -1,10 +1,8 @@
-import { execSync } from 'node:child_process'
 import colors from 'colors'
 import { commandPublish, commandVersion } from '@abmao/pkgs'
 
 console.log(`${colors.cyan.bold('release: start')} 🏗`);
 (async function () {
-  execSync('npm run build', { stdio: 'inherit' })
   await commandVersion()
   await commandPublish()
 })()
