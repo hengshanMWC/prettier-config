7a3899c21d10287912bc8cc242bdbe3be2eb573e
hengshanMWC <1819293933@qq.com>
2023-12-25 16:14:44 +0800

    feat(创建): 创建配置

--- test/specs/index.test.ts
+++
@@ -1,8 +0,0 @@
-import { describe, expect, test } from 'vitest'
-import { demo } from '../../src/index'
-
-describe('test', () => {
-  test('demo', () => {
-    expect(demo()).toBe('demo')
-  })
-})
